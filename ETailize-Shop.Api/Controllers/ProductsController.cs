002b52e63d7f0649c594039a2b803d5ee7d98329
Emmanuel Yirenkyi <yirenemma@gmail.com>
2023-01-16 16:07:48 +0000

    Add project files.

---
+++ ETailize-Shop.Api/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using ETailize_Shop.Api.Model;
+using ETailize_Shop.Api.Services;
+
+namespace ETailize_Shop.Api.Controllers
+{
+    public class ProductsController
+    {
+        public List<Product> Get()
+        {
+            var productService = new ProductService();
+            return productService.GetProducts();
+        }
+    }
+}
---
+++ ETailize-Shop.Api/Services/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using ETailize_Shop.Api.Model;
+
+namespace ETailize_Shop.Api.Services
+{
+    public class ProductService
+    {
+        public ProductService()
+        {
+            
+        }
+
+        public List<Product> GetProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = 1,
+                    Name = "Bicycle",
+                    Description = "Nice Bicycle",
+                    Price = 56.5M
+                },
+                new Product()
+                {
+                    Id = 2,
+                    Name = "Bicycle",
+                    Description = "Nice Bicycle",
+                    Price = 56.5M
+                },
+                new Product()
+                {
+                    Id = 3,
+                    Name = "Head Set",
+                    Description = "Nice Bicycle",
+                    Price = 56.5M
+                },
+                new Product()
+                {
+                    Id = 4,
+                    Name = "Laptop",
+                    Description = "Nice Bicycle",
+                    Price = 56.5M
+                },
+                new Product()
+                {
+                    Id = 5,
+                    Name = "IPhone",
+                    Description = "Nice Bicycle",
+                    Price = 56.5M
+                },
+            };
+        }
+    }
+}
